6c683bfe337998b530d1c9aff8a6ff9fc9e917f8
eric-owen <65836532+eric-owen@users.noreply.github.com>
2021-07-12 20:06:29 -0400

    post

--- test_crud/create.php
+++ test_crud/create.php
@@ -1,51 +1,102 @@
 <?php
 
-$pdo = new PDO('mysql:host=localhost;port=8889;dbname=products_crud', 'root', 'root');
+require_once "functions.php";
+
+$pdo = new PDO('mysql:host=localhost;port=3306;dbname=products_crud', 'root', 'root');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-var_dump($_POST);
+$errors = [];
+
+$title = '';
+$description = '';
+$price = '';
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+    $price = $_POST['price'];
+
+    $image = $_FILES['image'] ?? null;
+    $imagePath = '';
+
+    if (!is_dir('images')) {
+        mkdir('images');
+    }
+
+    if ($image && $image['tmp_name']) {
+        $imagePath = 'images/' . randomString(8) . '/' . $image['name'];
+        mkdir(dirname($imagePath));
+        move_uploaded_file($image['tmp_name'], $imagePath);
+    }
+
+    if (!$title) {
+        $errors[] = 'Product title is required';
+    }
+
+    if (!$price) {
+        $errors[] = 'Product price is required';
+    }
+
+    if (empty($errors)) {
+        $statement = $pdo->prepare("INSERT INTO products (title, image, description, price, create_date)
+                VALUES (:title, :image, :description, :price, :date)");
+        $statement->bindValue(':title', $title);
+        $statement->bindValue(':image', $imagePath);
+        $statement->bindValue(':description', $description);
+        $statement->bindValue(':price', $price);
+        $statement->bindValue(':date', date('Y-m-d H:i:s'));
+
+        $statement->execute();
+        header('Location: index.php');
+    }
+}
+
+
 ?>
 
 <!doctype html>
 <html lang="en">
 
 <head>
-
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
-    <title>Hello, world!</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link href="app.css" rel="stylesheet" />
+    <title>Products CRUD</title>
 </head>
 
 <body>
-    <h1>Create New Product</h>
+    <h1>Create new Product</h1>
 
-        <form action="create.php" method="post">
-            <div class="mb-3">
-                <label>Product Image</label>
-                <input type="file" name="image">
+    <?php if (!empty($errors)) : ?>
+        <div class="alert alert-danger">
+            <?php foreach ($errors as $error) : ?>
+                <div><?php echo $error ?></div>
+            <?php endforeach; ?>
+        </div>
+    <?php endif; ?>
 
-            </div>
-            <div class="mb-3">
-                <label>Product Title</label>
-                <input type="text" class="form-control" name="title">
+    <form method="post" enctype="multipart/form-data">
+        <div class="form-group">
+            <label>Product Image</label><br>
+            <input type="file" name="image">
+        </div>
+        <div class="form-group">
+            <label>Product title</label>
+            <input type="text" name="title" class="form-control" value="<?php echo $title ?>">
+        </div>
+        <div class="form-group">
+            <label>Product description</label>
+            <textarea class="form-control" name="description"><?php echo $description ?></textarea>
+        </div>
+        <div class="form-group">
+            <label>Product price</label>
+            <input type="number" step=".01" name="price" class="form-control" value="<?php echo $price ?>">
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
 
-            </div>
-            <div class="mb-3">
-                <label>Description</label>
-                <textarea class="form-control" name="description"></textarea>
-
-            </div>
-            <div class="mb-3">
-                <label>Product Price</label>
-                <input type="number" step=".01" class="form-control" name="price">
-
-            </div>
-
-            <button type="submit" class="btn btn-primary">Submit</button>
-        </form>
 </body>
 
 </html>
